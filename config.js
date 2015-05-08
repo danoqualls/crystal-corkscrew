8ce5b510a9fab22196a3066d21aae510b0a8153c
danoqualls <mynameisdano@gmail.com>
2015-05-08 09:49:22 -0400

    first actual commit

---
+++ config.js
@@ -0,0 +1,77 @@
+// Corkscrew configuration
+var Corkscrew = {
+  name : "Style Guide &middot; Fresh Tilled Soil",
+  logo: "project-name/images/logo.jpg",
+  partials : "project-name/partials/",
+  repo: "https://github.com/freshtilledsoil/corkscrew",
+  css: ["project-name/css/client.css"],
+  js: ["project-name/js/client.js"],
+  htmlViewer: true,
+  sections : [
+    "base",
+    "components",
+    "grid",
+    "forms"
+  ],
+  templates : [
+    {
+      name: "Homepage",
+      location: "http://freshtilledsoil.com/"
+    },
+    {
+      name: "General content - post",
+      location: "http://www.freshtilledsoil.com/aux-in-a-nutshell/"
+    },
+    {
+      name: "General content - page",
+      location: "http://www.freshtilledsoil.com/about/"
+    },
+    {
+      name: "Category listing",
+      location: "http://www.freshtilledsoil.com/category/podcast/"
+    }
+  ],
+  colors : [
+    {
+      hex: "#278496",
+      label: "Primary Blue"
+    },
+    {
+      hex: "#0D346B",
+      label: "Primary Dark Blue"
+    },
+    {
+      hex: "#4BB0CE",
+      label: "Primary Light Blue"
+    },
+    {
+      hex: "#1E1E1E",
+      label: "Black"
+    },
+    {
+      hex: "#414141",
+      label: "Dark Gray"
+    },
+    {
+      hex: "#BAC5C6",
+      label: "Gray"
+    },
+    {
+      hex: "#F3F3F1",
+      label: "Light Gray"
+    },
+    {
+      hex: "#293352",
+      label: "Accent Blue"
+    },
+    {  hex: "#FD716B",
+      label: "Action Orange"
+    },
+    {
+      hex: "#E4F8F8",
+      label: "Accent Light Blue"
+    },
+   
+    
+  ]
+};
